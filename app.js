c7f171681a884de26fa7e439e414315e2082dd97
andriiglukhyi <andrigluxii@gmail.com>
2017-12-29 09:13:09 -0800

    new changes

--- app.js
+++ app.js
@@ -1,23 +1,15 @@
 'use strict'
 
-var questionArray = [
-  // fill in
-  [],
-  [],
-  [],
-  []
-];
+var questionArray = ['questin1', 'question2', 'qestion3' ];
+
+var answears1 = ['answear11', 'answear12', 'answear13'];
+
+var answears2 = ['answear21', 'answear22', 'answear23'];
+
+var answears3 = ['answear31', 'answear32', 'answear33'];
 
 
-function Question(scenerio, a1, a2, a3) {
-  this.scenerio = scenerio;
-  this.a1 = a1;
-  this.a2 = a2;
-  this........
-  //continue
-};
-
-
+function Question(question, answear1, answear2, answear3)
 for (var i = 0; i < questionArray.length; i++) {
   var q[i] = new Question(questionArray[i]);
 };
