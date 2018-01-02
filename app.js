f4a576e6d482dcc093985053427094296d9b92e1
warlordlizard <alexjbowers88@gmail.com>
2018-01-02 12:18:40 -0800

    lightsaber, results gauge, and images fixed

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ var form = document.getElementById('form1');
 // answers.push(answer1);
 form.addEventListener('submit', getanswers);
 
+var score = 65;
 
 function getanswers(){
   event.preventDefault();
   var data = [];
   var data1 = [];
-  var score = 60;
+  score = 65;
   for(var i=1; i<11; i++){
     var answer = document.getElementsByName('answer'+[i]);
     data.push(answer);
@@ -25,8 +26,10 @@ function getanswers(){
   }
   for(var i=0; i<data1.length; i++ ){
     score += data1[i];
-    console.log(score);
   }
+  var scaledScore = score / 130;
+  gauge.value(scaledScore);
+  console.log(score);
 }
 
 
@@ -37,3 +40,75 @@ function getanswers(){
 //   console.log(result);
 //   return result;
 // }
+
+// #### Gauge
+
+// The Gauge object encapsulates the behavior
+// of simple gauge. Most of the implementation
+// is in the CSS rules, but we do have a bit
+// of JavaScript to set or read the gauge value
+
+function Gauge(el) {
+
+    // ##### Private Properties and Attributes
+
+    var element,      // Containing element for the info component
+        data,         // `.gauge__data` element
+        needle,       // `.gauge__needle` element
+        value = 0.0,  // Current gauge value from 0 to 1
+        prop;         // Style for transform
+
+    // ##### Private Methods and Functions
+
+    var setElement = function(el) {
+        // Keep a reference to the various elements and sub-elements
+        element = el;
+        data = element.querySelector(".gauge__data");
+        needle = element.querySelector(".gauge__needle");
+    };
+
+    var setValue = function(x) {
+        value = x;
+        var turns = -0.5 + (x * 0.5);
+        data.style[prop] = "rotate(" + turns + "turn)";
+        needle.style[prop] = "rotate(" + turns + "turn)";
+    };
+
+    // ##### Object to be Returned
+
+    function exports() { };
+
+    // ##### Public API Methods
+
+    exports.element = function(el) {
+        if (!arguments.length) { return element; }
+        setElement(el);
+        return this;
+    };
+
+    exports.value = function(x) {
+        if (!arguments.length) { return value; }
+        setValue(x);
+        return this;
+    };
+
+    // ##### Initialization
+
+    var body = document.getElementsByTagName("body")[0];
+    ["webkitTransform", "mozTransform", "msTransform", "oTransform", "transform"].
+        forEach(function(p) {
+            if (typeof body.style[p] !== "undefined") { prop = p; }
+        }
+    );
+
+    if (arguments.length) {
+        setElement(el);
+    }
+
+    return exports;
+
+};
+
+var gauge = new Gauge(document.getElementById("gauge"));
+
+gauge.value(0.5);
